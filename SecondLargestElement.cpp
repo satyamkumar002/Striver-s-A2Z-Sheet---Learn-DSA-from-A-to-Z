f3a7b575a3eb7eeed1422be1b4ece0294f70c4aa
Satyam Kumar <sasmitsatyam2006@gmail.com>
2026-07-17 15:54:39 +0530

    Add files via upload

---
+++ SecondLargestElement.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int secondLargestElement(vector<int>& nums) {
+
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for (int i = 0; i < nums.size(); i++) {
+
+        if (nums[i] > largest) {
+            secondLargest = largest;
+            largest = nums[i];
+        }
+        else if (nums[i] > secondLargest && nums[i] != largest) {
+            secondLargest = nums[i];
+        }
+    }
+
+    if (secondLargest == INT_MIN) {
+        return -1;
+    }
+
+    return secondLargest;
+}
