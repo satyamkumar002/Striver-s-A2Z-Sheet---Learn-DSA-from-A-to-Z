e0aa6af5f9b163ea09163ee71022945f9806cd38
Satyam Kumar <sasmitsatyam2006@gmail.com>
2026-07-17 11:20:04 +0530

    Add files via upload

---
+++ Largest_Element.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int largestElement(vector<int> arr, int n) {
+    int largest = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    return largest;
+}
